900636ed4c16d2f5ec918e8b88535c50ac9c1bc1
lefkelisgreg <it21451@hua.gr>
2024-01-29 22:54:47 +0200

    GUI CALENDAR COMMIT GREG

--- src/main/java/gr/hua/dit/oop2/calendar/CalendarGUI.java
+++ src/main/java/gr/hua/dit/oop2/calendar/CalendarGUI.java
@@ -1,232 +1,155 @@
 package gr.hua.dit.oop2.calendar;
 
-import biweekly.Biweekly;
-import biweekly.ICalendar;
-import biweekly.component.VEvent;
-import biweekly.component.VTodo;
-import biweekly.property.*;
-import gr.hua.dit.oop2.calendar.Calendar;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import javax.swing.*;
-import java.awt.*;
-import java.io.IOException;
 
-import javax.swing.*;
-import java.io.IOException;
+public class CalendarGUI extends Application {
 
-public class CalendarGUI extends JFrame {
-
-    private JTextArea textArea;
-
-    public CalendarGUI() {
-        // Set up the frame
-        super("iCalendar App");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
-
-        // Create a text area to display events and tasks
-        textArea = new JTextArea();
-        textArea.setEditable(false);
-
-        // Create a scroll pane to hold the text area
-        JScrollPane scrollPane = new JScrollPane(textArea);
-
-        // Add the scroll pane to the frame
-        getContentPane().add(scrollPane);
-
-        // Load and display events for the current day (you may want to change the file path)
-        try {
-            displayDayEvents("your_ical_file.ics");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void displayDayEvents(String filePath) throws IOException {
-        // Create an instance of your Calendar class
-        Calendar calendar = new Calendar();
-
-        // Call the method to find and display events for the current day
-        calendar.findDayEvents(filePath);
-
-        // Get the events and tasks as strings
-        StringBuilder displayText = new StringBuilder();
-        for (VEvent event : calendar.sortedEvents()) {
-            displayText.append(getEventString(event)).append("\n");
-        }
-        for (VTodo task : calendar.sortedTasks()) {
-            displayText.append(getTaskString(task)).append("\n");
-        }
-
-        // Display the events and tasks in the text area
-        textArea.setText(displayText.toString());
-    }
-
-    private String getEventString(VEvent event) {
-        // Customize this method to format the event information as needed
-        return "Event: " + event.getSummary().getValue() +
-                "\nStart Date and Time: " + event.getDateStart().getValue() +
-                "\nEnd Date and Time: " + (event.getDateEnd() != null ? event.getDateEnd().getValue() : "") +
-                "\nDescription: " + event.getDescription().getValue() +
-                "\n-----------------------";
-    }
-
-    private String getTaskString(VTodo task) {
-        // Customize this method to format the task information as needed
-        return "Task: " + task.getSummary().getValue() +
-                "\nDeadline of the task: " + task.getDateDue().getValue() +
-                "\nStatus of the task: " + task.getStatus().getValue() +
-                "\nDescription: " + task.getDescription().getValue() +
-                "\n-----------------------";
-    }
-
-    private void displayMonthEvents(String filePath) throws IOException {
-        // Create an instance of your Calendar class
-        Calendar calendar = new Calendar();
-
-        // Call the method to find and display events for the current month
-        calendar.findMonthEvents(filePath);
-
-        // Get the events and tasks as strings
-        StringBuilder displayText = new StringBuilder();
-        for (VEvent event : calendar.sortedEvents()) {
-            displayText.append(getEventString(event)).append("\n");
-        }
-        for (VTodo task : calendar.sortedTasks()) {
-            displayText.append(getTaskString(task)).append("\n");
-        }
-
-        // Display the events and tasks in the text area
-        textArea.setText(displayText.toString());
-    }
-
-    private void displayPastDayEvents(String filePath) throws IOException {
-        // Create an instance of your Calendar class
-        Calendar calendar = new Calendar();
-
-        // Call the method to find and display events for the past day
-        calendar.findPastDay(filePath);
-
-        // Get the events and tasks as strings
-        StringBuilder displayText = new StringBuilder();
-        for (VEvent event : calendar.getSortedEvents()) {
-            displayText.append(getEventString(event)).append("\n");
-        }
-        for (VTodo task : calendar.getSortedTasks()) {
-            displayText.append(getTaskString(task)).append("\n");
-        }
-
-        // Display the events and tasks in the text area
-        textArea.setText(displayText.toString());
-    }
-
-    private void displayPastWeekEvents(String filePath) throws IOException {
-        // Create an instance of your Calendar class
-        Calendar calendar = new Calendar();
-
-        // Call the method to find and display events for the past week
-        calendar.findPastWeek(filePath);
-
-        // Get the events and tasks as strings
-        StringBuilder displayText = new StringBuilder();
-        for (VEvent event : calendar.getSortedEvents()) {
-            displayText.append(getEventString(event)).append("\n");
-        }
-        for (VTodo task : calendar.getSortedTasks()) {
-            displayText.append(getTaskString(task)).append("\n");
-        }
-
-        // Display the events and tasks in the text area
-        textArea.setText(displayText.toString());
-    }
-
-    private void displayPastMonthEvents(String filePath) throws IOException {
-        // Create an instance of your Calendar class
-        Calendar calendar = new Calendar();
-
-        // Call the method to find and display events for the past month
-        calendar.findPastMonth(filePath);
-
-        // Get the events and tasks as strings
-        StringBuilder displayText = new StringBuilder();
-        for (VEvent event : calendar.getSortedEvents()) {
-            displayText.append(getEventString(event)).append("\n");
-        }
-        for (VTodo task : calendar.getSortedTasks()) {
-            displayText.append(getTaskString(task)).append("\n");
-        }
-
-        // Display the events and tasks in the text area
-        textArea.setText(displayText.toString());
-    }
-
-    private void displayToDoEvents(String filePath) throws IOException {
-        // Create an instance of your Calendar class
-        Calendar calendar = new Calendar();
-
-        // Call the method to find and display todo tasks
-        calendar.findToDoEvents(filePath);
-
-        // Get the tasks as strings
-        StringBuilder displayText = new StringBuilder();
-        for (VTodo task : calendar.getSortedTasks()) {
-            displayText.append(getTaskString(task)).append("\n");
-        }
-
-        // Display the tasks in the text area
-        textArea.setText(displayText.toString());
-    }
-
-    private void displayDueEvents(String filePath) throws IOException {
-        // Create an instance of your Calendar class
-        Calendar calendar = new Calendar();
-
-        // Call the method to find and display due events
-        calendar.findDueEvents(filePath);
-
-        // Get the tasks as strings
-        StringBuilder displayText = new StringBuilder();
-        for (VTodo task : calendar.getSortedTasks()) {
-            displayText.append(getTaskString(task)).append("\n");
-        }
-
-        // Display the tasks in the text area
-        textArea.setText(displayText.toString());
-    }
-
-    private void createNewEventTask(String filePath) throws IOException {
-        // Create an instance of your Calendar class
-        Calendar calendar = new Calendar();
-
-        // Call the method to allow the user to create a new event or task
-        calendar.createEvent(filePath);
-
-        // Refresh the display after creating a new event/task
-        displayAllEvents(filePath);
-    }
-
-    private void displayAllEvents(String filePath) throws IOException {
-        // Method to display all events, you can implement it as per your requirements
-    }
+    private List<Event> events = new ArrayList<>();
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            CalendarGUI calendarGUI = new CalendarGUI();
-            calendarGUI.setVisible(true);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Calendar App");
+
+        // UI Components
+        ListView<Event> eventListView = new ListView<>();
+        Button loadButton = new Button("Load Events");
+        Button addButton = new Button("Add Event");
+        Button editButton = new Button("Edit Event");
+        Button completeButton = new Button("Complete Event");
+
+        // Event Listeners
+        loadButton.setOnAction(e -> loadEvents(primaryStage, eventListView));
+        addButton.setOnAction(e -> addEvent(primaryStage, eventListView));
+        editButton.setOnAction(e -> editEvent(primaryStage, eventListView));
+        completeButton.setOnAction(e -> completeEvent(eventListView));
+
+        // Layout
+        VBox vbox = new VBox(loadButton, eventListView, addButton, editButton, completeButton);
+        Scene scene = new Scene(vbox, 400, 300);
+
+        // Reminder thread
+        Thread reminderThread = new Thread(() -> {
+            while (true) {
+                checkReminders();
+                try {
+                    Thread.sleep(60000); // Check every minute
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
         });
+        reminderThread.setDaemon(true);
+        reminderThread.start();
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void loadEvents(Stage primaryStage, ListView<Event> eventListView) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Events File");
+        File file = fileChooser.showOpenDialog(primaryStage);
+
+        if (file != null) {
+            // TODO: Read events from the file and populate the events list
+            // For now, let's add some dummy data
+            events.add(new Event("Event 1", LocalDateTime.now().plusDays(1)));
+            events.add(new Event("Event 2", LocalDateTime.now().plusDays(2)));
+            events.add(new Event("Event 3", LocalDateTime.now().plusDays(3)));
+            updateEventListView(eventListView);
+        }
+    }
+
+    private void addEvent(Stage primaryStage, ListView<Event> eventListView) {
+        // TODO: Implement the logic to add a new event
+        // You can use a dialog to get user input for the new event
+        // For now, let's add a dummy event
+        events.add(new Event("New Event", LocalDateTime.now().plusDays(1)));
+        updateEventListView(eventListView);
+    }
+
+    private void editEvent(Stage primaryStage, ListView<Event> eventListView) {
+        // TODO: Implement the logic to edit an existing event
+        // You can use a dialog to get user input for editing the event
+        // For now, let's edit the first event
+        if (!events.isEmpty()) {
+            Event firstEvent = events.get(0);
+            firstEvent.setName("Edited Event");
+            updateEventListView(eventListView);
+        }
+    }
+
+    private void completeEvent(ListView<Event> eventListView) {
+        // TODO: Implement the logic to mark an event as completed
+        // For now, let's complete the first event
+        if (!events.isEmpty()) {
+            Event firstEvent = events.get(0);
+            firstEvent.setCompleted(true);
+            updateEventListView(eventListView);
+        }
+    }
+
+    private void checkReminders() {
+        LocalDateTime now = LocalDateTime.now();
+        for (Event event : events) {
+            if (!event.isCompleted() && now.isAfter(event.getDateTime())) {
+                System.out.println("Reminder: Event '" + event.getName() + "' is upcoming!");
+            }
+        }
+    }
+
+    private void updateEventListView(ListView<Event> eventListView) {
+        eventListView.getItems().clear();
+        eventListView.getItems().addAll(events);
+    }
+
+    private static class Event {
+        private String name;
+        private LocalDateTime dateTime;
+        private boolean completed;
+
+        public Event(String name, LocalDateTime dateTime) {
+            this.name = name;
+            this.dateTime = dateTime;
+            this.completed = false;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public LocalDateTime getDateTime() {
+            return dateTime;
+        }
+
+        public boolean isCompleted() {
+            return completed;
+        }
+
+        public void setCompleted(boolean completed) {
+            this.completed = completed;
+        }
+
+        @Override
+        public String toString() {
+            return name + " - " + dateTime + (completed ? " (Completed)" : "");
+        }
     }
 }
